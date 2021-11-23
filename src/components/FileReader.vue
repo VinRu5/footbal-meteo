ba3948a0f3ef8ff444fd1b2f9c8223c20785e171
VinRu5 <vincenzorusso.inbox@gmail.com>
2021-11-23 18:31:08 +0100

    football

--- src/components/FileReader.vue
+++ src/components/FileReader.vue
@@ -1,20 +1,29 @@
 <template>
-  <label class="text-reader">
-    <input type="file" @change="loadTextFromFile">
-  </label>
+  <input type="file" class="input-style" @change="loadTextFromFile">
 </template>
 
 <script>
 export default {
     name: 'FileReader',
   methods: {
+    //constructor FileReader per leggere il file in input
     loadTextFromFile(ev) {
+      //prendo il file dall'input
       const file = ev.target.files[0];
+      //genero l'oggetto reader
       const reader = new FileReader();
-
+      //al caricamento invio il file per l'elaborazione nel componente padre
       reader.onload = e => this.$emit("load", e.target.result);
+      //leggo il file
       reader.readAsText(file);
     }
   }
 };
 </script>
+
+<style scoped>
+  .input-style{
+    margin: 32px;
+    text-align: center;
+  }
+</style>
--- src/components/Football.vue
+++ src/components/Football.vue
@@ -1,8 +1,17 @@
 <template>
-  <div class="row">
-    <button @click="showTeam()">Click</button>
+  <div class="row justify-content-around align-items-center row-height">
+    <h2 class="text-center">Carica il tuo file per determinare la squadra con la minor differenza reti</h2>
     <FileReader @load="text = $event"></FileReader>
-  <div>{{ winnerTeam }}</div>
+    <button 
+      class="col-3 btn btn-light"
+      @click="showTeam()"
+    >
+      Click
+    </button>
+  <div v-if="showResult()">
+    <div class="text-center mt-4">La minor differenza reti è di:</div>
+    <div class="text-center">{{ winnerTeam.name }} con una differenza reti di {{ winnerTeam.scoredGoals - winnerTeam.concededGoals }}</div>
+  </div>
   </div>
 </template>
 
@@ -14,25 +23,31 @@ export default {
 
   data: () => ({
     text: "",
-    winnerTeam: "",
+    winnerTeam: {},
   }),
   components: {
     FileReader,
   },
 
   computed: {
+    //Array contenente le squadre di calcio 
     teams() {
-      //console.log(this.text)
+      //Creo un array di linee di testo
       const lines = this.text
         .split("\n")
         .map((str) => str.replace(".", "").replace("-", ""));
+      //elimino le stringhe vuote
       lines.splice(0, 1);
+      lines.splice(21, 1);
+      lines.splice(17, 1);
 
+      //trasformo ogni linea in un Array
       const arrayLines = lines
         .map((str) => str.split(/\s/g))
         .map((array) => array.filter((str) => str.length > 0));
 
       let teams = [];
+      //trasformo i dati delle squdre in oggetti
       arrayLines.forEach((line) => {
         teams.push({
           id: parseInt(line[0]),
@@ -53,27 +68,39 @@ export default {
   },
 
   methods: {
-    showTeam() {
+    
+    showTeam() { //funzione per determinare la squadre con minor differenza reti
+
+
         if(this.teams.length === 0) {
             this.winnerTeam = '';
         } else {
             
             let winner;
+            let teamWinner;
 
             this.teams.forEach(team=> {
                 let difference = Math.abs(team.scoredGoals - team.concededGoals);
-                //console.log(difference, 'difference', team.id)
+
                 if(typeof winner === 'undefined' || winner > difference) {
-                    winner = difference
-                    //TODO da sistemare
+                
+              
+                    winner = difference;
+                    teamWinner = team;
+
                 }
             })
-            console.log(typeof winner, 'winner')
+            this.winnerTeam = teamWinner;
         }
+    },
+
+    showResult() {
+      return Object.keys(this.winnerTeam).length === 0 ? false : true;
     }
   },
 };
 </script>
 
-<style>
+<style scoped>
+
 </style>
